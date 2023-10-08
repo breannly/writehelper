58ec3f30ba28cbb1fb96552fdb70ccdc115fb883
Armen Katvalyan <armenkatvalyan@192-168-0-61.local>
2023-10-08 20:04:35 +0300

    delete unused

--- src/main/java/com/writershelper/mapper/PostMapper.java
+++ src/main/java/com/writershelper/mapper/PostMapper.java
@@ -1,29 +1,14 @@
 package com.writershelper.mapper;
 
-import com.google.common.base.Throwables;
 import com.writershelper.dto.post.PostCreateDto;
 import com.writershelper.model.Label;
 import com.writershelper.model.Post;
 import com.writershelper.model.Status;
 import com.writershelper.model.Writer;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.*;
 
 public class PostMapper {
-
-    public static Optional<Post> map(ResultSet rs) {
-        return mapItems(rs).stream().findFirst();
-    }
-
-    public static List<Post> mapList(ResultSet rs) {
-        return mapItems(rs);
-    }
-
-    public static Post lazyMap(ResultSet rs) {
-        return mapItem(rs);
-    }
 
     public static Post map(PostCreateDto request,
                            List<Label> labels,
@@ -40,43 +25,5 @@ public class PostMapper {
         post.setUpdated(date);
         post.setStatus(status);
         return post;
-    }
-
-    private static List<Post> mapItems(ResultSet rs) {
-        Map<Long, Post> postsStore = new HashMap<>();
-
-        try {
-            while (rs.next()) {
-                long postId = rs.getLong("p_id");
-                Post post = postsStore.computeIfAbsent(postId, id -> mapItem(rs));
-
-                long labelId = rs.getLong("l_id");
-                if (labelId != 0L) {
-                    Label label = LabelMapper.map(rs);
-                    post.getLabels().add(label);
-                }
-
-            }
-        } catch (SQLException e) {
-            System.out.println(Throwables.getStackTraceAsString(e));
-            throw new RuntimeException("Failed to map ResultSet to Writer", e);
-        }
-
-        return new ArrayList<>(postsStore.values());
-    }
-
-    private static Post mapItem(ResultSet rs) {
-        try {
-            Post post = new Post();
-            post.setId(rs.getLong("p_id"));
-            post.setContent(rs.getString("p_content"));
-            post.setCreated(rs.getDate("p_created"));
-            post.setUpdated(rs.getDate("p_updated"));
-            post.setStatus(Status.valueOf(rs.getString("p_status")));
-            post.setLabels(new ArrayList<>());
-            return post;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 }
